f00af876f7791205f434a88ab176fe1e4d243cf3
MJN <marcel-jan.niemczyk@stud.hslu.ch>
2020-09-20 01:10:03 +0200

    add holes. add mole. add player

--- Assets/player/scripts/MovePlayer.cs
+++ Assets/player/scripts/MovePlayer.cs
@@ -6,62 +6,115 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 10f;
+    private float speed = 1f;
+
+    GameObject hole1;
+    GameObject hole2;
+    GameObject hole3;
+    GameObject hole4;
+    GameObject hole5;
+    GameObject hole6;
+    GameObject hole7;
+    GameObject hole8;
+    GameObject hole9;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Not needed
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.Alpha1))
         {
-            MoveForward();
+            MoveToHole1();
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.Alpha2))
         {
-            MoveBackwards();
+            MoveToHole2();
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.Alpha3))
         {
-            MoveLeft();
+            MoveToHole3();
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.Alpha4))
         {
-            MoveRight();
+            MoveToHole4();
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Alpha5))
         {
-            MoveUp();
+            MoveToHole5();
+        }
+
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            MoveToHole6();
+        }
+
+        if (Input.GetKey(KeyCode.Alpha7))
+        {
+            MoveToHole7();
+        }
+
+        if (Input.GetKey(KeyCode.Alpha8))
+        {
+            MoveToHole8();
+        }
+
+        if (Input.GetKey(KeyCode.Alpha9))
+        {
+            MoveToHole9();
         }
     }
-
-    private void MoveForward()
+    
+    private void MoveToHole1()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
-    private void MoveBackwards()
-    {
-        transform.Translate(Vector3.back * speed * Time.deltaTime);
-    }
-    private void MoveLeft()
-    {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
-    }
-    private void MoveRight()
-    {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
-    }
-    private void MoveUp()
-    {
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        //transform.position(new Vector3(gameObject.transform.position));
     }
 
+    private void MoveToHole2()
+    {
+
+    }
+
+    private void MoveToHole3()
+    {
+
+    }
+
+    private void MoveToHole4()
+    {
+
+    }
+
+    private void MoveToHole5()
+    {
+
+    }
+
+    private void MoveToHole6()
+    {
+
+    }
+
+    private void MoveToHole7()
+    {
+
+    }
+
+    private void MoveToHole8()
+    {
+
+    }
+
+    private void MoveToHole9()
+    {
+
+    }
 }

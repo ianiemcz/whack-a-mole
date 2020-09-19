5c1e3858d710176709388bdc1ad58310a9a0fe1b
MJN <marcel-jan.niemczyk@stud.hslu.ch>
2020-09-19 23:14:42 +0200

    first assets and setup

---
+++ Assets/player/scripts/MovePlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveForward();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveBackwards();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            MoveUp();
+        }
+    }
+
+    private void MoveForward()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    private void MoveBackwards()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+    private void MoveLeft()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+    private void MoveRight()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+    private void MoveUp()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+}
